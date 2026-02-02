eb6a58d51c1a75979d7e175067b2541e937203d2
jonnurik <78033038+jonnurik@users.noreply.github.com>
2026-02-02 20:24:11 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,41 @@
 import os
+import logging
 from telegram import Update
 from telegram.ext import (
     ApplicationBuilder,
     CommandHandler,
+    MessageHandler,
     ContextTypes,
+    filters,
 )
+
+logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 
+# /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot ishlayapti ✅")
 
 
-async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Yordam komandasi")
+# HAR QANDAY MATN
+async def check_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text.lower()
+
+    if "xato" in text or "yomon" in text:
+        await update.message.reply_text("❌ Iltimos, xato so‘z ishlatmang!")
+
+    print("Kelgan matn:", text)
 
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("help", help_cmd))
+
+    # 👇 ENG MUHIM
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_text))
 
     app.run_polling()
 
