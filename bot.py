5a1dd5b090bc75b169c9f74bb7019ad23bd728b5
jonnurik <78033038+jonnurik@users.noreply.github.com>
2026-02-02 19:37:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,14 @@
-
 import os
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-from spellchecker import SpellChecker
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
-spell = SpellChecker(language=None)
-
-# Load Uzbek dictionary
-with open("uz_words.txt", "r", encoding="utf-8") as f:
-    spell.word_frequency.load_words(f.read().split())
-
-async def check_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text.lower().split()
-    mistakes = spell.unknown(text)
-
-    if mistakes:
-        reply = []
-        for word in mistakes:
-            correction = spell.correction(word)
-            reply.append(f"❌ {word} → ✅ {correction}")
-        await update.message.reply_text("\n".join(reply))
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Imlo tekshiruvchi bot ishga tushdi ✅")
+    await update.message.reply_text("Bot ishladi ✅")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, check_message))
+app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
